24f2047f948c781e773484e186a62341f7a88715
rekhanayak277 <rekhanayak277@gmail.com>
2025-08-29 21:07:41 +0530

    Commit 01

--- src/Second_Program.java
+++ src/Second_Program.java
@@ -1,7 +1,4 @@
-import java.sql.SQLOutput;
-
-public class First_Program {
-
+public class Second_Program {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Learning Java");

ef813744ae836c16efc8d54e994144f7282e1e55
rekhanayak277 <rekhanayak277@gmail.com>
2025-09-11 09:25:34 +0530

    Commit 02

---
+++ src/LoopPrograms/FizzBuzzNumber_IfElse.java
@@ -0,0 +1,37 @@
+package LoopPrograms;
+
+public class FizzBuzzNumber_IfElse {
+
+    public static void main(String[] args) {
+
+        /*
+        If divisible by 2 - Fizz
+        If divisible by 5 - Buzz
+        If divisible by both 2 & 5- FizzBuzz (should be multiplier of 10)
+        If none, put that number in else statement
+         */
+
+        for (int n = 1; n <= 50; n++) //initialization
+        {
+
+             if (n%10==0) //Condition
+        {
+            System.out.println(n+"-FizzBuzz"); //statement
+        }
+
+
+         else if (n%2==0) //Condition
+        {
+                System.out.println(n+"-Fizz");//statement
+
+            }
+
+         else if (n%5==0) //Condition
+            {
+                 System.out.println(n+"-Buzz"); //statement
+               }
+
+         else {
+            System.out.println(n + "- not divisible by 2 or 5"); //statement
+        }
+    }}}
---
+++ src/LoopPrograms/NumberOfDigitsInAGivenNumber.java
@@ -0,0 +1,9 @@
+package LoopPrograms;
+
+public class NumberOfDigitsInAGivenNumber {
+
+    /* Given number is - 14578
+    Total digits in the number -5
+    Use
+     */
+}

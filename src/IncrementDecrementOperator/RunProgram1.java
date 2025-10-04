fbf339e8bdc5e9782ec662100b187e92951a601c
rekhanayak277 <rekhanayak277@gmail.com>
2025-10-04 21:04:26 +0530

    Commit 04

---
+++ src/IncrementDecrementOperator/RunProgram1.java
@@ -0,0 +1,19 @@
+package IncrementDecrementOperator;
+
+public class RunProgram1 {
+
+
+    public static void main(String[] args) {
+
+        int a = 11;
+       // a = 11 / 12
+        int b = ++a;
+        // b = 12
+        System.out.println(a);
+        System.out.println(b);
+
+
+    }
+
+
+}
---
+++ src/IncrementDecrementOperator/RunProgram2.java
@@ -0,0 +1,17 @@
+package IncrementDecrementOperator;
+
+public class RunProgram2 {
+
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        // a=10 / 11
+        int b = a++;
+        // 10
+        System.out.println(a);
+        System.out.println(b);
+
+
+    }
+}
